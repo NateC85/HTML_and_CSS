3ba6538627bf4a01ee84fad4cc7807d95934aa0f
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-09-13 10:21:02 -0600

    new JS

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -3,5 +3,13 @@
 	<script>
 		alert("Press if you dare!");
 	</script>
+	<script>
+	function My_First_Function()  {
+	var string="Kiss me, I'm Irish!";
+	var result=string.fontcolor("green");
+	document.getElementById("Irish").innerHTML=
+	result;
 	
+	}
+	</script>
 </html>
